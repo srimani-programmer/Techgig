d3ddce0a915e5839e9536475210a4c28c402d0aa
srimani-programmer <srimani.scholar@gmail.com>
2019-04-19 14:02:24 +0530

    Final commit for the solution.

---
+++ Languages/Java/FirstNFirst.java
@@ -0,0 +1,13 @@
+
+import java.io.*;
+import java.util.*;
+
+class FirstNFirst{
+    public static void main(String[] args) throws Exception {
+        String s = "";
+        Scanner sc = new Scanner(System.in);
+        s += sc.nextLine();
+        sc.close();
+        System.out.print(s.charAt(0));
+    }
+}
